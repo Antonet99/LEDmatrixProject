c02b6b035bc56f73d22284f8553de8b9b0f0e347
Antonet99 <antoniobaio90@gmail.com>
2023-07-23 15:23:05 +0200

    mqtt connection

--- SOD23/src/WiFiConnection.cpp
+++ SOD23/src/WiFiConnection.cpp
@@ -1,14 +1,15 @@
 
 #include "headers/constants.hpp"
 #include "headers/wifiConnection.hpp"
+#include "headers/constMqtt.hpp"
 
-
-void wifiConn(){
+void wifiConn()
+{
   Serial.begin(9600);
-  Serial.print("********** connecting to WIFI : ");
+  Serial.print("********** connecting to WIFI :");
   Serial.println(ssid);
 
-  WiFi.begin(ssid,wifi_password);
+  WiFi.begin(ssid, wifi_password);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
@@ -18,7 +19,25 @@ void wifiConn(){
   Serial.println("->WiFi connected");
   Serial.println("->IP address: ");
   Serial.println(WiFi.localIP());
- }
-void mqttConn(){
-  
+}
+void mqttConn()
+{
+  Serial.print("********** connecting to MQTT : ");
+  Serial.println(mqtt_server);
+
+  client.setServer(mqtt_server, mqtt_port);
+  while (!client.connected())
+  {
+    Serial.println("********** connecting to MQTT...");
+    if (client.connect(mqtt_clientID, mqtt_username, mqtt_password))
+    {
+      Serial.println("ESP32 connected to MQTT server");
+    }
+    else
+    {
+      Serial.print("Connection failed with state ");
+      Serial.print(client.state());
+      delay(500);
+    }
+  }
 }
--- SOD23/src/headers/constMqtt.hpp
+++ SOD23/src/headers/constMqtt.hpp
@@ -1,6 +1,6 @@
-const char *mqtt_username = "sod23";                  // my mqtt username
-const char *mqtt_password = "sod23";                  // my mqtt password
-const char *mqtt_pubTopic = "publish/esp32/motion";   // publish/username/apiKeyIn
-const char* mqtt_clientID = "esp32_sod"; 
-const char *mqtt_server = "192.168.0.105";
-unsigned int mqtt_port = 1883; 
+const char *mqtt_username = "sod23";                // my mqtt username
+const char *mqtt_password = "sod23";                // my mqtt password
+const char *mqtt_pubTopic = "publish/esp32/motion"; // publish/username/apiKeyIn
+const char *mqtt_clientID = "esp32_sod";            // my clientID
+const char *mqtt_server = "192.168.0.105";          // my mqtt server address
+unsigned int mqtt_port = 1883;                      // my mqtt port
